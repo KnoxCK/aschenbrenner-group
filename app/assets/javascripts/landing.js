6dca4893d8080fb631ff1c38194f2ec929973e71
Clarissa Knox <knox.ck@gmail.com>
2017-09-27 18:14:04 +0100

    prepare for heroku

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	setTimeout(function() {
     $(".coming-soon").addClass("fadeOut");
-	}, 3000);
+	}, 3400);
 	setTimeout(function() {
     $(".coming-soon").addClass("hidden");
     $(".address").removeClass("hidden");
-	}, 4000);
+	}, 4400);
 })
